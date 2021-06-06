a1d8a91c19f565051758813a829e57897671e73c
denislavivanov <72037017+denislavivanov@users.noreply.github.com>
2021-06-06 21:45:33 +0300

    Update Texture.java

--- Graphics/Texture.java
+++ Graphics/Texture.java
@@ -13,14 +13,19 @@ public class Texture {
         stbi_set_flip_vertically_on_load(true);
         data = stbi_load(path, width, height, nrChannels, 0);
 
+        if(data == null || data.remaining() == 0)
+            System.out.println("Texture failed to load!");
+
         ID = glGenTextures();
         glBindTexture(GL_TEXTURE_2D, ID);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width[0], height[0], 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width[0], height[0], 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
+
+        stbi_image_free(data);
     }
 
     public void bind() {
